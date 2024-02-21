a4fedcefbfdebbe77f35ae06f8696ab268250308
Roan Creed <RoanC14@Gmail.com>
2024-02-21 13:06:16 +0000

    Initial Commit

---
+++ TestSkiNav/TestSkiNavApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSkiNavApp.swift
+//  TestSkiNav
+//
+//  Created by Roan Creed on 21/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct TestSkiNavApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
